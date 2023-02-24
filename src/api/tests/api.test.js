7bbb49ff48becdccec2c1b677dcfc43c9e871434
macdaegeon <chleorjs37@gmail.com>
2023-02-24 13:03:12 +0900

    api test 진행

--- src/api/tests/api.test.js
+++ src/api/tests/api.test.js
@@ -1,7 +1,22 @@
-import axios from 'axios';
+import { _axios } from '@/utils/axios';
 import { todoList } from '../index';
-describe('api 테스트 실시', () => {
-  test('todolist 호출 ', async () => {
-    const spyGet = jest.spyOn(axios, 'get');
+describe('todoList api 실시', () => {
+  test('todolist 호출 및 결과 비교하기', async () => {
+    _axios.get = jest.fn().mockResolvedValue([
+      { Idx: 1, ToDoItem: 'css 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 2, ToDoItem: 'Java 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 3, ToDoItem: 'Javascript 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 4, ToDoItem: 'Spring 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 5, ToDoItem: 'TypeScript 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+    ]);
+    const result = await todoList();
+
+    expect(result).toEqual([
+      { Idx: 1, ToDoItem: 'css 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 2, ToDoItem: 'Java 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 3, ToDoItem: 'Javascript 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 4, ToDoItem: 'Spring 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+      { Idx: 5, ToDoItem: 'TypeScript 공부', createdAt: '2023-02-01T15:00:00.000Z', updatedAt: '2023-02-01T15:00:00.000Z' },
+    ]);
   });
 });
