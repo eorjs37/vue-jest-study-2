50586352ec7b61be5caf74b22775ff5aa09e11fe
macdaegeon <chleorjs37@gmail.com>
2023-03-19 12:10:45 +0900

    todowrite 진행중

--- src/views/ToDoWriteView.vue
+++ src/views/ToDoWriteView.vue
@@ -3,7 +3,9 @@
 </template>
 
 <script>
-export default {};
+export default {
+  setup() {},
+};
 </script>
 
 <style></style>
---
+++ src/views/tests/todowrite.test.js
@@ -0,0 +1,8 @@
+/**
+ * todowrite.test.js
+ 1. input에 글을 작성한다.
+ 2. input에 enter 또는 완료 버튼을 누르면 값이 비어있는지 체크 후 있으면 
+ */
+describe('todowrite testing', () => {
+  test('input에 값이 있는지 확인한다.', async () => {});
+});
